33bc82ff5262df901b844ed8c7c7ad072797aa00
Sara Ford <saraford@users.noreply.github.com>
2017-11-16 16:43:30 -0800

    added new Hello route

---
+++ HelloAPI-server/Controllers/HelloController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HelloAPI_server.Controllers
+{
+    public class HelloController : ApiController
+    {
+        [Route("hello")]
+        [HttpGet]
+        public string Hello()
+        {
+            return "Hello there!";
+        }
+    }
+}
