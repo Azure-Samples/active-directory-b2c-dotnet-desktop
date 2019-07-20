8ddd76a52eafb84a9f40778e718f02302953d11b
Jenny Ferries <jeferrie@microsoft.com>
2019-07-19 17:54:20 -0700

    updates to app

--- active-directory-b2c-wpf/App.xaml.cs
+++ active-directory-b2c-wpf/App.xaml.cs
@@ -1,4 +1,31 @@
-﻿using System.Windows;
+﻿//------------------------------------------------------------------------------
+//
+// Copyright (c) Microsoft Corporation.
+// All rights reserved.
+//
+// This code is licensed under the MIT License.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//------------------------------------------------------------------------------
+
+using System.Windows;
 using Microsoft.Identity.Client;
 
 namespace active_directory_b2c_wpf
@@ -8,19 +35,21 @@ namespace active_directory_b2c_wpf
     /// </summary>
     public partial class App : Application
     {
-        private static string Tenant = "fabrikamb2c.onmicrosoft.com";
-        private static string ClientId = "841e1190-d73a-450c-9d68-f5cf16b78e81";
-        public static string PolicySignUpSignIn = "b2c_1_susi";
-        public static string PolicyEditProfile = "b2c_1_edit_profile";
-        public static string PolicyResetPassword = "b2c_1_reset";
+        private static readonly string Tenant = "b2cDemo2019Tenant.onmicrosoft.com";
+        public static readonly string ClientId = "f3557bf1-2d23-4537-a79f-8569c1876fb2"; //"841e1190-d73a-450c-9d68-f5cf16b78e81";
+        public static string PolicySignUpSignIn = "B2C_1_signup_signin";
+        public static string PolicyEditProfile = "B2C_1_edit_profile";
+        public static string PolicyResetPassword = "B2C_1_password_reset";
+        public static string PolicyROPC = "B2C_1_ropc";
 
-        public static string[] ApiScopes = { "https://fabrikamb2c.onmicrosoft.com/helloapi/demo.read" };
-        public static string ApiEndpoint = "https://fabrikamb2chello.azurewebsites.net/hello";
+        public static string[] ApiScopes = { "https://b2cDemo2019Tenant.onmicrosoft.com/api/hello.read" };
+        public static string ApiEndpoint = "https://b2cDemo2019Tenant.onmicrosoft.com/api";
 
-        private static string BaseAuthority = "https://fabrikamb2c.b2clogin.com/tfp/{tenant}/{policy}/";
+        private static string BaseAuthority = "https://b2cDemo2019Tenant.b2clogin.com/tfp/{tenant}/{policy}/";
         public static string Authority = BaseAuthority.Replace("{tenant}", Tenant).Replace("{policy}", PolicySignUpSignIn);
         public static string AuthorityEditProfile = BaseAuthority.Replace("{tenant}", Tenant).Replace("{policy}", PolicyEditProfile);
         public static string AuthorityResetPassword = BaseAuthority.Replace("{tenant}", Tenant).Replace("{policy}", PolicyResetPassword);
+        public static string AuthorityROPCPassword = BaseAuthority.Replace("{tenant}", Tenant).Replace("{policy}", PolicyROPC);
 
         public static IPublicClientApplication PublicClientApp { get; private set; }
 
--- active-directory-b2c-wpf/MainWindow.xaml.cs
+++ active-directory-b2c-wpf/MainWindow.xaml.cs
@@ -1,4 +1,31 @@
-﻿using Microsoft.Identity.Client;
+﻿//------------------------------------------------------------------------------
+//
+// Copyright (c) Microsoft Corporation.
+// All rights reserved.
+//
+// This code is licensed under the MIT License.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//------------------------------------------------------------------------------
+
+using Microsoft.Identity.Client;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -10,6 +37,8 @@ using System.Windows;
 using System.Windows.Interop;
 using Newtonsoft.Json.Linq;
 using System.Text;
+using System.Security;
+using System.Windows.Controls;
 
 namespace active_directory_b2c_wpf
 {
@@ -40,7 +69,7 @@ namespace active_directory_b2c_wpf
                 DisplayBasicTokenInfo(authResult);
                 UpdateSignInState(true);
             }
-            catch (MsalServiceException ex)
+            catch (MsalException ex)
             {
                 try
                 {
@@ -74,9 +103,8 @@ namespace active_directory_b2c_wpf
             var app = App.PublicClientApp;
             try
             {
-                ResultText.Text = $"Calling API:{App.AuthorityEditProfile}";
+                ResultText.Text = $"Calling Edit Profile Policy:\n{App.AuthorityEditProfile}";
                 AuthenticationResult authResult = await (app as PublicClientApplication).AcquireTokenInteractive(App.ApiScopes)
-                            .WithAccount(GetAccountByPolicy(accounts, App.PolicySignUpSignIn))
                             .WithParentActivityOrWindow(new WindowInteropHelper(this).Handle)
                             .WithB2CAuthority(App.AuthorityEditProfile)
                             .WithPrompt(Prompt.NoPrompt)
@@ -86,7 +114,7 @@ namespace active_directory_b2c_wpf
             }
             catch (Exception ex)
             {
-                ResultText.Text = $"Session has expired, please sign out and back in.{App.AuthorityEditProfile}{Environment.NewLine}{ex}";
+                ResultText.Text = $"Session has expired, please sign out and back in.\n{App.AuthorityEditProfile}{Environment.NewLine}{ex}";
             }
         }
 
@@ -128,6 +156,67 @@ namespace active_directory_b2c_wpf
                 ResultText.Text = await GetHttpContentWithToken(App.ApiEndpoint, authResult.AccessToken);
                 DisplayBasicTokenInfo(authResult);
             }
+        }
+
+        private async void ROPCButton_Click(object sender, RoutedEventArgs e)
+        {
+            AuthenticationResult authResult = null;
+            var app = App.PublicClientApp;
+
+            string username = UsernameTextBox.Text;
+
+            SecureString password = ConvertToSecureString(PasswordTextBox);
+
+            try
+            {
+                ResultText.Text = "";
+                authResult = await (app as PublicClientApplication).AcquireTokenByUsernamePassword(
+                    App.ApiScopes,
+                    username,
+                    password)
+                    .WithB2CAuthority(App.AuthorityROPCPassword)
+                    .ExecuteAsync();
+
+                DisplayBasicTokenInfo(authResult);
+                UpdateSignInState(true);
+            }
+            catch (MsalClientException ex)
+            {
+                try
+                {
+                    if (ex.Message.Contains("AADB2C90118"))
+                    {
+                        authResult = await (app as PublicClientApplication).AcquireTokenInteractive(App.ApiScopes)
+                            .WithParentActivityOrWindow(new WindowInteropHelper(this).Handle)
+                            .WithPrompt(Prompt.SelectAccount)
+                            .WithB2CAuthority(App.AuthorityResetPassword)
+                            .ExecuteAsync();
+                    }
+                    else
+                    {
+                        ResultText.Text = $"Error Acquiring Token:{Environment.NewLine}{ex}";
+                    }
+                }
+                catch (Exception)
+                {
+                }
+            }
+            catch (Exception ex)
+            {
+                ResultText.Text = $"Error Acquiring Token:{Environment.NewLine}{ex}";
+            }
+        }
+
+        private SecureString ConvertToSecureString(TextBox text)
+        {
+            if (text.Text.Length > 0)
+            {
+                SecureString securePassword = new SecureString();
+                text.Text.ToCharArray().ToList().ForEach(p => securePassword.AppendChar(p));
+                securePassword.MakeReadOnly();
+                return securePassword;
+            }
+            return null;
         }
 
         /// <summary>
@@ -181,6 +270,7 @@ namespace active_directory_b2c_wpf
                 EditProfileButton.Visibility = Visibility.Visible;
                 SignOutButton.Visibility = Visibility.Visible;
 
+                ROPCButton.Visibility = Visibility.Collapsed;
                 SignInButton.Visibility = Visibility.Collapsed;
             }
             else
@@ -193,6 +283,7 @@ namespace active_directory_b2c_wpf
                 SignOutButton.Visibility = Visibility.Collapsed;
 
                 SignInButton.Visibility = Visibility.Visible;
+                ROPCButton.Visibility = Visibility.Visible;
             }
         }
 
@@ -204,13 +295,15 @@ namespace active_directory_b2c_wpf
                 JObject user = ParseIdToken(authResult.IdToken);
 
                 TokenInfoText.Text += $"Name: {user["name"]?.ToString()}" + Environment.NewLine;
-                TokenInfoText.Text += $"Identity Provider: {user["idp"]?.ToString()}" + Environment.NewLine;
-                TokenInfoText.Text += $"Tenant Id: {authResult.TenantId}" + Environment.NewLine;
+                TokenInfoText.Text += $"City: {user["city"]?.ToString()}" + Environment.NewLine;
+                TokenInfoText.Text += $"Job Title: {user["jobTitle"]?.ToString()}" + Environment.NewLine;
                 var emails = user["emails"] as JArray;
                 if (emails != null)
                 {
                     TokenInfoText.Text += $"Emails: {emails[0].ToString()}" + Environment.NewLine;
                 }
+                TokenInfoText.Text += $"Identity Provider: {user["iss"]?.ToString()}" + Environment.NewLine;
+                TokenInfoText.Text += $"Policy: {user["tfp"]?.ToString()}" + Environment.NewLine;
             }
         }
 
@@ -238,7 +331,7 @@ namespace active_directory_b2c_wpf
                 var app = App.PublicClientApp;
                 IEnumerable<IAccount> accounts = await App.PublicClientApp.GetAccountsAsync();
 
-                AuthenticationResult authResult = await app.AcquireTokenSilent(App.ApiScopes, 
+                AuthenticationResult authResult = await app.AcquireTokenSilent(App.ApiScopes,
                                                                                GetAccountByPolicy(accounts, App.PolicySignUpSignIn))
                     .ExecuteAsync();
 
