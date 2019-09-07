49a5200d8332f43218da539b5ded9b5ddde9c185
Jenny Ferries <jeferrie@microsoft.com>
2019-09-06 18:31:19 -0700

    add localhost:5000 add logs

--- active-directory-b2c-wpf/App.xaml.cs
+++ active-directory-b2c-wpf/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows;
 using Microsoft.Identity.Client;
 
 namespace active_directory_b2c_wpf
@@ -9,14 +13,14 @@ namespace active_directory_b2c_wpf
     public partial class App : Application
     {
         private static readonly string Tenant = "fabrikamb2c.onmicrosoft.com";
-        private static readonly string AzureAdB2CHostname = "login.microsoftonline.com";
+        private static readonly string AzureAdB2CHostname = "fabrikamb2c.b2clogin.com"; //"login.microsoftonline.com"; //
         private static readonly string ClientId = "841e1190-d73a-450c-9d68-f5cf16b78e81";
         public static string PolicySignUpSignIn = "b2c_1_susi";
         public static string PolicyEditProfile = "b2c_1_edit_profile";
         public static string PolicyResetPassword = "b2c_1_reset";
 
         public static string[] ApiScopes = { "https://fabrikamb2c.onmicrosoft.com/helloapi/demo.read" };
-        public static string ApiEndpoint = "https://fabrikamb2chello.azurewebsites.net/hello";
+        public static string ApiEndpoint = "http://localhost:5000/hello";
 
         private static string AuthorityBase = $"https://{AzureAdB2CHostname}/tfp/{Tenant}/";
         public static string AuthoritySignInSignUp = $"{AuthorityBase}{PolicySignUpSignIn}";
@@ -29,9 +33,19 @@ namespace active_directory_b2c_wpf
         {
             PublicClientApp = PublicClientApplicationBuilder.Create(ClientId)
                 .WithB2CAuthority(AuthoritySignInSignUp)
+                .WithLogging(Log, LogLevel.Verbose, true)
                 .Build();
 
             TokenCacheHelper.Bind(PublicClientApp.UserTokenCache);
+        }
+
+        private static void Log(LogLevel level, string message, bool containsPii)
+        {
+            string logs = ($"{level} {message}");
+            StringBuilder sb = new StringBuilder();
+            sb.Append(logs);
+            File.AppendAllText(System.Reflection.Assembly.GetExecutingAssembly().Location + ".msalLogs.txt", sb.ToString());
+            sb.Clear();
         }
     }
 }
